7e66e59e0f1c132f26bac500eac59bb14ca00285
antudas22 <20antu20@gmail.com>
2022-11-10 00:06:55 +0600

    post reviews api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express()
@@ -17,7 +17,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
-        const serviceCollection = client.db('antuWorks').collection('services')
+        const serviceCollection = client.db('antuWorks').collection('services');
+        const reviewCollection = client.db('antuWorks').collection('reviews');
         app.get('/services', async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -29,6 +30,20 @@ async function run(){
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/service/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+
         })
     }
     finally{
