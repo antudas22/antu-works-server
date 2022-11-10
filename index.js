66d11c9c82cad286613fe073213b4a1d30dec79b
antudas22 <20antu20@gmail.com>
2022-11-10 16:01:01 +0600

    Show latest services on all services page

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run(){
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
-            res.send(services);
+            res.send(services.reverse());
         })
 
         app.get('/service/:id', async(req, res) => {
